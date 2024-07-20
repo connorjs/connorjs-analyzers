7a23697be6860c3fa12f03cef1b8177e6e8320a6
Connor Sullivan <dev.connorjs@gmail.com>
2024-07-19 20:16:38 -0400

    Explicitly use *.sln?

--- ci-build.ps1
+++ ci-build.ps1
@@ -1,9 +1,9 @@
 $env:CI = true
 
 try {
-	dotnet restore
-	dotnet build -c release --no-restore
-	dotnet pack -c release --no-build
+	dotnet restore *.sln
+	dotnet build -c release --no-restore *.sln
+	dotnet pack -c release --no-build *.sln
 } finally {
 	$env:CI = $null
 }
